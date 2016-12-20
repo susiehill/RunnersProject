b1e267e882c0b32f80915ed03f48b12c75afe863
dwilliam24 <dwilliamesquivel@gmail.com>
2016-12-19 16:45:10 -0800

    Login Reg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def index
+
+  end
+  def create
+    if Runner.create(first_name:params[:first_name],last_name:params[:last_name],email:params[:email],password:params[:password],gender:params[:gender],shoe_size:params[:shoe_size])
+      redirect_to '/'
+    end
+  end
+  def login
+    r = Runner.find_by_email(params[:email])
+      if ( r && r.authenticate(params[:password]))
+        session[:user_id] = r.id
+        redirect_to '/sessions/new'
+      else
+
+        redirect_to '/'  
+  end
 end
