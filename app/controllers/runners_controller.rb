a93af8244d1e7b820012b5c4476588716402c9dc
Andrew Yang <andrewyang1@gmail.com>
2016-12-20 16:50:01 -0800

    Added Update routes and method

--- app/controllers/runners_controller.rb
+++ app/controllers/runners_controller.rb
@@ -4,9 +4,16 @@ class RunnersController < ApplicationController
     @runner = Runner.find(params[:id])
   end
   def edit
+  	@runner=Runner.find(params[:id])
   end
   def leaderboard
   	@runner=Runner.order(shoe_size: :desc)
   end
-
+  def update
+    r=Runner.find(session[:user_id])
+    if r.update(first_name: params[:first_name], last_name: params[:last_name],shoe_size: params[:shoe_size],gender: params[:gender],email: params[:email])
+      redirect_to "/runners/#{r.id}"
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-get "/" => "sessions#index"
-post "/create" => "sessions#create"
-post "/login" => "sessions#login"
-get "/logout" => "sessions#destroy"
-get "/leaderboard" => "runners#leaderboard"
-get "/runners/:id" => "runners#profile"
-get "/runners/:id/edit" => "runners#edit"
+	get "/" => "sessions#index"
+	post "/create" => "sessions#create"
+	post "/login" => "sessions#login"
+	get "/logout" => "sessions#destroy"
+	get "/leaderboard" => "runners#leaderboard"
+	get "/runners/:id/edit" => "runners#edit"
+	get "/runners/:id" => "runners#profile"
+	patch "/runners" => "runners#update"
 end
